12c72eaff988364501fa0b53bac974c1340beaca
Pellissier <dav.pellissier@gmail.com>
2020-12-05 16:29:59 +0100

    SeparateChaining

---
+++ HashMapSeparateChaining.h
@@ -0,0 +1,37 @@
+
+
+#ifndef ASD2_LABS_2020_HASHMAPSEPARATECHAINING_H
+#define ASD2_LABS_2020_HASHMAPSEPARATECHAINING_H
+
+
+template<typename T>
+class HashMapSeparateChaining {
+
+    //std::unordered_set<T> hashMap;
+
+public:
+
+    void insert(const T& key) {
+
+    }
+
+    bool contains(const T& key) {
+
+    }
+
+    void erase(const T& key) {
+
+    }
+
+    size_t size() {
+
+    }
+};
+
+
+
+
+
+
+
+#endif //ASD2_LABS_2020_HASHMAPSEPARATECHAINING_H
