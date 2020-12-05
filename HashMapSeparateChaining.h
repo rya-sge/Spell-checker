a19670dd8e24a83e57b0cdf216878c98b641e98f
Pellissier <dav.pellissier@gmail.com>
2020-12-05 17:08:09 +0100

    Contains & Size

--- HashMapSeparateChaining.h
+++ HashMapSeparateChaining.h
@@ -4,19 +4,37 @@
 #define ASD2_LABS_2020_HASHMAPSEPARATECHAINING_H
 
 
+#include <cstdlib>
+#include <vector>
+#include <forward_list>
+#include <algorithm>
+
 template<typename T>
 class HashMapSeparateChaining {
 
-    //std::unordered_set<T> hashMap;
+    typedef std::forward_list<T> Bucket;
+    typedef std::vector<Bucket> HashMap;
+    HashMap hashMap;
+
+    // Valeurs par défaut
+    size_t M = 2;
+    size_t N = 0;
+
+private:
+    size_t hash(T key) {
+        return std::hash<T>()(key) % hashMap.size();
+    }
 
 public:
+
 
     void insert(const T& key) {
 
     }
 
     bool contains(const T& key) {
-
+        size_t index = hash(key);
+        return (std::find(hashMap.at(index).begin(), hashMap.at(index).end(), key) == hashMap.at(index).end());
     }
 
     void erase(const T& key) {
@@ -24,7 +42,7 @@ public:
     }
 
     size_t size() {
-
+        return N;
     }
 };
 
