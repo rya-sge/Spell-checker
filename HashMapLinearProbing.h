7138d71435923dffdd6266e0889b7bc54586dbe2
Pellissier <dav.pellissier@gmail.com>
2020-12-05 18:00:33 +0100

    Insert & contains de LP

--- HashMapLinearProbing.h
+++ HashMapLinearProbing.h
@@ -6,13 +6,16 @@
 
 template<typename T>
 class HashMapLinearProbing {
+// Valeurs par défaut
+    size_t M = 2;
+    size_t N = 0;
+    double MAX_FACTOR = 0.5;
+    double MIN_FACTOR = (double)1/8;
 
     typedef std::vector<T> HashMap;
     HashMap hashMap;
 
-    // Valeurs par défaut
-    size_t M = 2;
-    size_t N = 0;
+
 
     const size_t EMPTY_VALUE = 0;
 
@@ -21,23 +24,37 @@ private:
         return std::hash<T>()(key) % M;
     }
 
+
+
 public:
 
-
     void insert(const T& key) {
-        // TODO: check if rehash is needed
+        if((double)N / M >= MAX_FACTOR){
+            // todo realloc
+        }
 
-        size_t index = hash(key);
         // find first available index
+        size_t index = hash(key);
         while(hashMap.at(index) != EMPTY_VALUE && hashMap.at(index) != key){
                 index = (index + 1)%M;
         }
         hashMap.at(index) = key;
-
-
     }
 
     bool contains(const T& key) {
+        size_t index = hash(key);
+        size_t count = 0;
+
+        while(count != N){
+            if(hashMap.at(index) == key)
+                return true;
+            else if(!hashMap.at(index))
+                break;
+
+            index = (index + 1)%M;
+            ++count;
+        }
+
     }
 
     void erase(const T& key) {
