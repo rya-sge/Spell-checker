148ae1c750e4f75fb2968fd5808680f9dfdd0891
ryanHEIG <ryan.sauge@gmail.com>
2020-12-22 15:06:13 +0100

    HashCode version

--- HashMapLinearProbing.h
+++ HashMapLinearProbing.h
@@ -4,15 +4,29 @@
 #include <cstdlib>
 #include <vector>
 
+template<typename K>
+class HashNode{
+public:
+   // V value;
+    K key;
+
+    //Constructor of hashnode
+    HashNode(K key)
+    {
+       // this->value = value;
+        this->key = key;
+    }
+};
 template<typename T>
 class HashMapLinearProbing {
 // Valeurs par défaut
     size_t M = 2;
     size_t N = 0;
     double MAX_FACTOR = 0.5;
-    double MIN_FACTOR = (double)1/8;
+    double MIN_FACTOR = 1.0/8;
 
-    typedef std::vector<T> HashMap;
+    typedef std::vector<HashNode<T>> HashMap;
+    //typedef std::vector<std::pair<int, T>> HashMap;
     HashMap hashMap;
 
 
@@ -35,10 +49,10 @@ public:
 
         // find first available index
         size_t index = hash(key);
-        while(hashMap.at(index) != EMPTY_VALUE && hashMap.at(index) != key){
-                index = (index + 1)%M;
+        while(hashMap.at(index) != NULL && hashMap.at(index).key != key){
+            index = (index + 1)%M;
         }
-        hashMap.at(index) = key;
+        hashMap.at(index) = new HashNode<T>(key);
     }
 
     bool contains(const T& key) {
@@ -46,7 +60,7 @@ public:
         size_t count = 0;
 
         while(count != N){
-            if(hashMap.at(index) == key)
+            if(hashMap.at(index) != NULL && hashMap.at(index).value == key)
                 return true;
             else if(!hashMap.at(index))
                 break;
@@ -58,6 +72,30 @@ public:
     }
 
     void erase(const T& key) {
+        if(!contains(key)) return;
+        int i = hash(key);
+        while(hashMap.at(i) == NULL || hashMap.at(i).value != key ){
+            i = (i + 1) % M;
+        }
+        delete hashMap.at(i); //Suppression objet pointé
+        hashMap.at(i) = NULL;
+        i = (i + 1) % M;
+        while(hashMap.at(i) != NULL)
+        {
+            T keyToRedo = hashMap.at(i).value;
+            //Value valToRedo = vals[i];
+            delete hashMap.at(i); //Suppression objet pointé
+            hashMap.at(i) = NULL;
+           // vals[i] = null;
+            N--;
+            insert(keyToRedo);
+            i = (i + 1) % M;
+
+        }
+        N--;
+        if(N > 0 && N <= MIN_FACTOR){
+            //resize
+        }
 
     }
 
