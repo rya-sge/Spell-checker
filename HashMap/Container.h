46d821d67b91c4dc71055fe2e8a0d137a0c4b4b8
ryanHEIG <ryan.sauge@gmail.com>
2020-12-26 16:33:22 +0100

    lambda structure

---
+++ HashMap/Container.h
@@ -0,0 +1,42 @@
+//
+// Created by super on 26/12/2020.
+//
+
+#ifndef ASD2_LABS_2020_CONTAINER_H
+#define ASD2_LABS_2020_CONTAINER_H
+
+
+#include <functional>
+
+template<typename T>
+class C{
+
+    //Co container;
+
+    using boolFunction =  std::function<bool(const T& key)>;
+    using voidFunction =  std::function<void(const T& key)>;
+    const  boolFunction& containsF;
+    const  voidFunction& insertF;
+    const  voidFunction& eraseF;
+
+public:
+
+    C(const boolFunction& containsFunction, const voidFunction& insertFunction, const voidFunction& eraseFunction)
+            : containsF(containsFunction), insertF(insertFunction), eraseF(eraseFunction) {
+        //this->container = container;
+    }
+
+    bool contains(const T& key) const{
+        return containsF(key);
+    }
+
+    void insert(const T& key) const {
+        insertF(key);
+    }
+
+    void erase(const T& key) const {
+        eraseF(key);
+    }
+};
+
+#endif //ASD2_LABS_2020_CONTAINER_H
--- main2.cpp
+++ main2.cpp
@@ -11,7 +11,7 @@
 #include "HashMap/HashMapWrapper.h"
 #include "HashMap/HashMapLinearProbing.h"
 #include "HashMap/HashMapSeparateChaining.h"
-
+#include "HashMap/Container.h"
 using namespace std;
 
 #include <iostream>
@@ -19,44 +19,6 @@ using namespace std;
 #include <unordered_set>
 
 using namespace std;
-template<typename Co, typename T>
-class C{
-
-    //Co container;
-
-    //typedef std::function<bool(const T& key) const> boolFunction;
-    // typedef std::function<void(const T& key) const> voidFunction;
-    const  std::function<bool(const T& key)>& containsF;
-    const  std::function<void(const T& key)>& insertF;
-    const  std::function<void(const T& key)>& eraseF;
-
-    // voidFunction &insertF;
-    // voidFunction &eraseF;
-
-public:
-    /* C(const std::function<bool(const T& key)>& containsFunction) : containsF(containsFunction){
-
-     }*/
-
-    C(const std::function<bool(const T& key)>& containsFunction, const std::function<void(const T& key) >& insertFunction, const std::function<void(const T& key)> & eraseFunction)
-            : containsF(containsFunction), insertF(insertFunction), eraseF(eraseFunction) {
-        //this->container = container;
-    }
-
-    bool contains(const T& key) const{
-        return containsF(key);
-    }
-
-    void insert(const T& key) const {
-        insertF(key);
-    }
-
-    void erase(const T& key) const {
-        eraseF(key);
-    }
-};
-
-
 
 
 int main() {
@@ -65,13 +27,19 @@ int main() {
 
     Container test;
 
-    // C<Container, Type> d ([&test](const Type& key){ return test.find(key) != test.end();});
 
-    C<Container, Type> us ([&test](const Type& key){ return test.find(key) != test.end();},
+    C<Type> us ([&test](const Type& key){ return test.find(key) != test.end();},
                            [&test](const Type& key){ test.insert(key);},
                            [&test](const Type& key){ test.erase(key);}       );
 
-    return 0;
+    test.insert("lol");
+    cout << us.contains("lol");
+    us.erase("lol");
+
+    us.insert("Lol");
+    us.erase("Lol");
+    us.erase("V");
+    us.contains("Lol");
+return 0;
 }
 
-/*
