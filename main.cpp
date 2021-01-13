b31e603d8f8e5f62fff9c341555b9db086289929
Pellissier <dav.pellissier@gmail.com>
2021-01-13 17:28:27 +0100

    Tests propres dans main.cpp

--- main.cpp
+++ main.cpp
@@ -12,105 +12,139 @@
 #include "TernarySearchTree.h"
 
 #include <unordered_set>
-#include "AVLTree.h"
-
 using namespace std;
+
+
+typedef Container<string> ContainerWrapper;
+
+
+// Prototypes des fonctions pour pouvoir les retrouver plus facilement
+void testCommon(const string &INPUT_FILE, const Dictionary<ContainerWrapper> &DICO, long double timeDico);
+void testLinearProbing(const string& INPUT_FILE, const string& DICTIONARY_FILE);
+void testSeparateChaining(const string& INPUT_FILE, const string& DICTIONARY_FILE);
+void testUnorderedSet(const string& INPUT_FILE, const string& DICTIONARY_FILE);
+void testSortedVector(const string& INPUT_FILE, const string& DICTIONARY_FILE);
+void testTST(const string& INPUT_FILE, const string& DICTIONARY_FILE);
 
 
 int main() {
     const string DICTIONARY_FILE ="../Labo4_data/dictionary.txt";
     const string INPUT_FILE = "../Labo4_data/input_wikipedia.txt";
 
-    typedef Container<string> ContainerWrapper;
+    testSortedVector(INPUT_FILE, DICTIONARY_FILE);
 
-    chrono::high_resolution_clock::time_point begin, end;
-    long double timeDico, timeSpellCheck;
+    return 0;
+}
 
-    // Génération du dictionnaire
-    begin = chrono::high_resolution_clock::now();
-
-    // INFO : Pour tester, simplement enlever le "/*" du le titre de la section
-
-    /*// LINEAR PROBING
-    typedef HashMapLinearProbing<string> ContainerType;
-    ContainerType test;
-    ContainerWrapper cw ([&test](const string& key){ return test.contains(key);},
-                         [&test](const string& key){ test.insert(key);},
-                         [&test](const string& key){ test.erase(key);});
-    const Dictionary<ContainerWrapper> DICO(cw, DICTIONARY_FILE); //*/
-
-    // SEPARATE CHAINING
-    typedef HashMapLinearProbing<string> ContainerType;
-    ContainerType test;
-    ContainerWrapper cw ([&test](const string& key){ return test.contains(key);},
-                         [&test](const string& key){ test.insert(key);},
-                         [&test](const string& key){ test.erase(key);});
-    const Dictionary<ContainerWrapper> DICO(cw, DICTIONARY_FILE); //*/
-
-    /*// VECTEUR
-    typedef vector<string> ContainerType;
-    ContainerType test;
-    ContainerWrapper cw ([&test](const string& key){ return binary_search(test.begin(), test.end(), key) ;;},
-                         [&test](const string& key){ test.push_back(key);},
-                         [&test](const string& key){ test.erase(lower_bound(test.begin(), test.end(), key)); });
-
-    // Lecture du fichier
-    std::string line;
-    std::ifstream file;
-
-    file.open(DICTIONARY_FILE);
-
-    if(!file.is_open()){
-        std::cout << '\n' << "Erreur de lecture du fichier"; return EXIT_FAILURE;
-    }
-    while(!file.eof()){
-        std::getline(file, line);
-        std::istringstream iss(line);
-        std::string word;
-        while(getline(iss, word, ' ')){
-            // add every word
-            test.push_back(word);
-        }
-    }
-    file.close();
-    // Tri rapide
-    sort(test.begin(), test.end());
-    const Dictionary<ContainerWrapper> DICO(cw); //*/
-
-    /*// AVL
-    typedef AVLTree<string, char> ContainerType;
-    ContainerType test;
-    ContainerWrapper cw ([&test](const string& key){ return test.contains(key);},
-                         [&test](const string& key){ test.put(key, 0);},
-                         [&test](const string& key){ test.deleteElement(key);});
-    const Dictionary<ContainerWrapper> DICO(cw, DICTIONARY_FILE); //*/
-
-    /*// TST
-    typedef TST ContainerType;
-    ContainerType test;
-    ContainerWrapper cw ([&test](const string& key){return test.contains(key);},
-                         [&test](const string& key){ test.put(key, 1);},
-                         [&test](const string& key){ });
-    const Dictionary<ContainerWrapper> DICO(cw, DICTIONARY_FILE); //*/
-
-    end = chrono::high_resolution_clock::now();
-    timeDico = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
-    begin = chrono::high_resolution_clock::now();
+void testCommon(const string &INPUT_FILE, const Dictionary<ContainerWrapper> &DICO, long double timeDico){
+    chrono::high_resolution_clock::time_point begin = chrono::high_resolution_clock::now();
 
     // Spellcheck
     SpellChecker<ContainerWrapper> sp(INPUT_FILE, DICO);
     string res = sp.result();
+
     cout << res << endl;
     //sp.writeOutput("../output.txt");
 
-
-    end = chrono::high_resolution_clock::now();
-    timeSpellCheck = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
-
+    // done
+    chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
+    long double timeSpellCheck = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
 
     cout << "Dictionary generated in " << timeDico << "ms" << endl;
     cout << "Spellcheck done in " << timeSpellCheck << "ms" << endl;
     cout << "Total time : " << timeDico + timeSpellCheck << "ms" << endl;
+}
 
-    return 0;
+void testLinearProbing(const string& INPUT_FILE, const string& DICTIONARY_FILE){
+    chrono::high_resolution_clock::time_point begin = chrono::high_resolution_clock::now();
+
+    // Creation
+    HashMapLinearProbing<string> test;
+    ContainerWrapper cw([&test](const string& key){ return test.contains(key);},
+                        [&test](const string& key){ test.insert(key);},
+                        [&test](const string& key){ test.erase(key);});
+    const Dictionary<ContainerWrapper> DICO(cw, DICTIONARY_FILE);
+
+    // done
+    chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
+    long double timeDico = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
+
+
+    testCommon(INPUT_FILE, DICO, timeDico);
+}
+
+void testSeparateChaining(const string& INPUT_FILE, const string& DICTIONARY_FILE){
+    chrono::high_resolution_clock::time_point begin = chrono::high_resolution_clock::now();
+
+    // Creation
+    HashMapSeparateChaining<string> test;
+    ContainerWrapper cw ([&test](const string& key){ return test.contains(key);},
+                         [&test](const string& key){ test.insert(key);},
+                         [&test](const string& key){ test.erase(key);});
+    const Dictionary<ContainerWrapper> DICO(cw, DICTIONARY_FILE);
+
+    // done
+    chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
+    long double timeDico = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
+
+
+    testCommon(INPUT_FILE, DICO, timeDico);
+}
+
+void testUnorderedSet(const string& INPUT_FILE, const string& DICTIONARY_FILE){
+    chrono::high_resolution_clock::time_point begin = chrono::high_resolution_clock::now();
+
+    // Creation
+    unordered_set<string> test;
+    ContainerWrapper cw ([&test](const string& key){ return test.find(key) != test.end();},
+                         [&test](const string& key){ test.insert(key);},
+                         [&test](const string& key){ test.erase(key);}       );
+    const Dictionary<ContainerWrapper> DICO(cw, DICTIONARY_FILE);
+
+    //done
+    chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
+    long double timeDico = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
+
+
+    testCommon(INPUT_FILE, DICO, timeDico);
+}
+
+void testSortedVector(const string& INPUT_FILE, const string& DICTIONARY_FILE){
+    // Ici on insère les valeurs à la fin du vecteur puis on trie le vecteur.
+    // On peut se le permettre car on n'utilisera plus la fonction insert dans la suite des tests.
+    // À ne pas faire en pratique ! (le vecteur doit être trié en tout temps)
+    chrono::high_resolution_clock::time_point begin = chrono::high_resolution_clock::now();
+
+    // Creation
+    vector<string> test;
+    ContainerWrapper cw ([&test](const string& key){ return binary_search(test.begin(), test.end(), key) ;;},
+                         [&test](const string& key){ test.push_back(key);},
+                         [&test](const string& key){ test.erase(lower_bound(test.begin(), test.end(), key)); });
+    const Dictionary<ContainerWrapper> DICO(cw, DICTIONARY_FILE);
+    sort(test.begin(), test.end());
+
+    // done
+    chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
+    long double timeDico = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
+
+
+    testCommon(INPUT_FILE, DICO, timeDico);
+}
+
+void testTST(const string& INPUT_FILE, const string& DICTIONARY_FILE){
+    chrono::high_resolution_clock::time_point begin = chrono::high_resolution_clock::now();
+
+    // Creation
+    TST test;
+    ContainerWrapper cw ([&test](const string& key){return test.contains(key);},
+                         [&test](const string& key){ test.put(key, 1);},
+                         [&test](const string& key){ });
+    const Dictionary<ContainerWrapper> DICO(cw, DICTIONARY_FILE);
+
+    // done
+    chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
+    long double timeDico = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
+
+
+    testCommon(INPUT_FILE, DICO, timeDico);
 }
