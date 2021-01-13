a94e7bdc8fbcf7c7b182b6aba15d5447f7383289
MichaelRuckstuhl <michael.ruckstuhl@heig-vd.ch>
2021-01-13 17:29:50 +0100

    TST

--- TernarySearchTree.h
+++ TernarySearchTree.h
@@ -8,11 +8,14 @@
 
 #include <string>
 #include <limits>
+#include <list>
 
 class TST{
 private:
 
     static const size_t  NOT_ASSIGNED = std::numeric_limits<size_t>::max();
+
+    size_t sizeTST = 0;
 
     struct Node{
         size_t val = NOT_ASSIGNED;
@@ -36,7 +39,11 @@ private:
             x->right = put(x->right, key, val, d);
         else if(d < key.length() - 1)
             x->mid = put(x->mid, key, val, d+1);
-        else x->val = val;
+        else{
+            if(x->val == NOT_ASSIGNED)
+                ++sizeTST;
+            x->val = val;
+        }
         return x;
     }
 
@@ -49,19 +56,53 @@ private:
         else return x;
     }
 
+    void erase(Node* x, std::string key, size_t d, std::list<Node*>& listNodes) const{
+        listNodes.push_back(x);
+        char c = key.at(d);
+        if (c < x->c) erase(x->left,key,d,listNodes);
+        else if (c > x->c) erase(x->right,key,d,listNodes);
+        else if (d < key.length() - 1) erase(x->mid,key,d+1,listNodes);
+        else {
+            x->val = NOT_ASSIGNED;
+            return;
+        }
+    }
+
 public:
     TST() : root(nullptr) { };
-    void put(std::string key, size_t val){
+    void insert(std::string key, size_t val){
         root = put(root, key, val, 0);
     }
     bool contains(std::string key) const{
-        return get(key) != NOT_ASSIGNED;
-    }
-    size_t get(std::string key) const{
         Node* x = get(root, key, 0);
-        if (x == nullptr) return NOT_ASSIGNED;
-        return x->val;
+        if (x == nullptr) return false;
+        return x->val != NOT_ASSIGNED;
     }
+    void erase(std::string key){
+        if(!contains(key))
+            return;
+        --sizeTST;
+        std::list<Node*> listNodes;
+        erase(root, key, 0, listNodes);
+        Node* x;
+        while(!listNodes.empty()){
+            x = listNodes.back();
+            listNodes.pop_back();
+            if (x->mid == nullptr and x->left == nullptr and x->right == nullptr and x->val == NOT_ASSIGNED){
+                delete x->right;
+                delete x->mid;
+                delete x->left;
+                delete x;
+            }
+            else{
+                break;
+            }
+        }
+    }
+    size_t size(){
+        return sizeTST;
+    }
+
 
 };
 
